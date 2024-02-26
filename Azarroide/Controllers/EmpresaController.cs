e41fb2dcadc53df134d38019a1d6eb4634e0f05d
marioaraujougb <mario.araujo@ugb.edu.br>
2024-02-26 15:57:09 -0300

    Adicionar arquivos de projeto.

---
+++ Azarroide/Controllers/EmpresaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Azarroide.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Azarroide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Azarroide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
